c361869e645dcfb52135e57b076c7ab2feda81a4
Rhythm Bellic <92723976+Rhythmbellic@users.noreply.github.com>
2023-04-25 23:36:20 +0530

    Update food.py

--- food.py
+++ food.py
@@ -16,4 +16,91 @@ import os
 # Walk through pizza_steak directory and list number of files
 for dirpath, dirnames, filenames in os.walk("pizza_steak"):
   print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
-  
+# Another way to find out how many images are in a file
+num_steak_images_train = len(os.listdir("pizza_steak/train/steak"))
+
+num_steak_images_train
+# Get the class names (programmatically, this is much more helpful with a longer list of classes)
+import pathlib
+import numpy as np
+data_dir = pathlib.Path("pizza_steak/train/") # turn our training path into a Python path
+class_names = np.array(sorted([item.name for item in data_dir.glob('*')])) # created a list of class_names from the subdirectories
+print(class_names)
+# View an image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+
+def view_random_image(target_dir, target_class):
+  # Setup target directory (we'll view images from here)
+  target_folder = target_dir+target_class
+
+  # Get a random image path
+  random_image = random.sample(os.listdir(target_folder), 1)
+
+  # Read in the image and plot it using matplotlib
+  img = mpimg.imread(target_folder + "/" + random_image[0])
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off");
+
+  print(f"Image shape: {img.shape}") # show the shape of the image
+
+  return img
+# View a random image from the training dataset
+img = view_random_image(target_dir="pizza_steak/train/",
+                        target_class="steak")
+# View the img (actually just a big array/tensor)
+img
+# View the image shape
+img.shape # returns (width, height, colour channels)
+# Get all the pixel values between 0 & 1
+img/255. 
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Set the seed
+tf.random.set_seed(42)
+
+# Preprocess data (get all of the pixel values between 1 and 0, also called scaling/normalization)
+train_datagen = ImageDataGenerator(rescale=1./255)
+valid_datagen = ImageDataGenerator(rescale=1./255)
+
+# Setup the train and test directories
+train_dir = "pizza_steak/train/"
+test_dir = "pizza_steak/test/"
+
+# Import data from directories and turn it into batches
+train_data = train_datagen.flow_from_directory(train_dir,
+                                               batch_size=32, # number of images to process at a time 
+                                               target_size=(224, 224), # convert all images to be 224 x 224
+                                               class_mode="binary", # type of problem we're working on
+                                               seed=42)
+
+valid_data = valid_datagen.flow_from_directory(test_dir,
+                                               batch_size=32,
+                                               target_size=(224, 224),
+                                               class_mode="binary",
+                                               seed=42)
+# Create a model similar to model_1 but add an extra layer and increase the number of hidden units in each layer
+model_3 = tf.keras.Sequential([
+  tf.keras.layers.Flatten(input_shape=(224, 224, 3)), # dense layers expect a 1-dimensional vector as input
+  tf.keras.layers.Dense(100, activation='relu'), # increase number of neurons from 4 to 100 (for each layer)
+  tf.keras.layers.Dense(100, activation='relu'),
+  tf.keras.layers.Dense(100, activation='relu'), # add an extra layer
+  tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# Compile the model
+model_3.compile(loss='binary_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=["accuracy"])
+
+# Fit the model
+history_3 = model_3.fit(train_data,
+                        epochs=5,
+                        steps_per_epoch=len(train_data),
+                        validation_data=valid_data,
+                        validation_steps=len(valid_data))
+# Check out model_3 architecture
+model_3.summary()
