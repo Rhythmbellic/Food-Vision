9b319d9fb6ce41e2026c554aef862faa93d01afd
Rhythm Bellic <92723976+Rhythmbellic@users.noreply.github.com>
2023-08-22 01:49:20 +0530

    Update food.py

--- food.py
+++ food.py
@@ -20,7 +20,7 @@ for dirpath, dirnames, filenames in os.walk("pizza_steak"):
 num_steak_images_train = len(os.listdir("pizza_steak/train/steak"))
 
 num_steak_images_train
-# Get the class names (programmatically, this is much more helpful with a longer list of classes)
+# Get the class names
 import pathlib
 import numpy as np
 data_dir = pathlib.Path("pizza_steak/train/") # turn our training path into a Python path
@@ -32,7 +32,7 @@ import matplotlib.image as mpimg
 import random
 
 def view_random_image(target_dir, target_class):
-  # Setup target directory (we'll view images from here)
+  # Setup target directory
   target_folder = target_dir+target_class
 
   # Get a random image path
@@ -50,10 +50,10 @@ def view_random_image(target_dir, target_class):
 # View a random image from the training dataset
 img = view_random_image(target_dir="pizza_steak/train/",
                         target_class="steak")
-# View the img (actually just a big array/tensor)
+# View the img 
 img
 # View the image shape
-img.shape # returns (width, height, colour channels)
+img.shape 
 # Get all the pixel values between 0 & 1
 img/255. 
 import tensorflow as tf
@@ -62,7 +62,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # Set the seed
 tf.random.set_seed(42)
 
-# Preprocess data (get all of the pixel values between 1 and 0, also called scaling/normalization)
+# Preprocess data 
 train_datagen = ImageDataGenerator(rescale=1./255)
 valid_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -72,9 +72,9 @@ test_dir = "pizza_steak/test/"
 
 # Import data from directories and turn it into batches
 train_data = train_datagen.flow_from_directory(train_dir,
-                                               batch_size=32, # number of images to process at a time 
-                                               target_size=(224, 224), # convert all images to be 224 x 224
-                                               class_mode="binary", # type of problem we're working on
+                                               batch_size=32, 
+                                               target_size=(224, 224), 
+                                               class_mode="binary", 
                                                seed=42)
 
 valid_data = valid_datagen.flow_from_directory(test_dir,
@@ -82,12 +82,12 @@ valid_data = valid_datagen.flow_from_directory(test_dir,
                                                target_size=(224, 224),
                                                class_mode="binary",
                                                seed=42)
-# Create a model similar to model_1 but add an extra layer and increase the number of hidden units in each layer
+# Create a model 
 model_3 = tf.keras.Sequential([
-  tf.keras.layers.Flatten(input_shape=(224, 224, 3)), # dense layers expect a 1-dimensional vector as input
-  tf.keras.layers.Dense(100, activation='relu'), # increase number of neurons from 4 to 100 (for each layer)
+  tf.keras.layers.Flatten(input_shape=(224, 224, 3)), 
+  tf.keras.layers.Dense(100, activation='relu'), 
   tf.keras.layers.Dense(100, activation='relu'),
-  tf.keras.layers.Dense(100, activation='relu'), # add an extra layer
+  tf.keras.layers.Dense(100, activation='relu'), 
   tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
@@ -102,5 +102,5 @@ history_3 = model_3.fit(train_data,
                         steps_per_epoch=len(train_data),
                         validation_data=valid_data,
                         validation_steps=len(valid_data))
-# Check out model_3 architecture
+# Check out model architecture
 model_3.summary()
